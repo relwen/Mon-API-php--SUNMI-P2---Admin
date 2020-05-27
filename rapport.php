18923a1a262c6a8e417e4c0cf9afec0741de8b9b
root <root@localhost.localdomain>
2020-05-27 15:37:02 +0200

    New Rapport

--- rapport.php
+++ rapport.php
@@ -1,13 +1,31 @@
 <?php
 
+class Constants
+{
+    //DATABASE DETAILS
+    static $DB_SERVER="localhost";
+    static $DB_NAME="u909898786_microstart";
+    static $USERNAME="u909898786_microstart226";
+    static $PASSWORD="microstart226";
 
-    function bdd()
+    //STATEMENTS
+    static $SQL_SELECT_ALL="SELECT * FROM clients";
+}
+
+class Spacecrafts
+{
+    /*******************************************************************************************************************************************/
+    /*
+       1.CONNECT TO DATABASE.
+       2. RETURN CONNECTION OBJECT
+    */
+    public function connect()
     {
         try
         {
             $pdo_options[PDO::ATTR_ERRMODE]=PDO::ERRMODE_EXCEPTION;
             
-            $bdd=new PDO('mysql:host=localhost;dbname=u909898786_microstart','u909898786_microstart226','microstart226',$pdo_options);
+            $con=new PDO('mysql:host=localhost;dbname=u909898786_microstart','u909898786_microstart226','microstart226',$pdo_options);
             
 
         }
@@ -17,65 +35,73 @@
             die('Erreur: ' . $e->getMessage());
         }
         
-        return $bdd;
+        return $con;
         
-
     }
-    
-    $bdd=bdd();
-    
-    
-    
-    		$requete=$bdd->prepare('SELECT * FROM agents WHERE phone=:phone');
-    		
-    		# Execution de la fonction
-    		$requete->execute(array('phone'=>"5756351"));
-    		$reponse=$requete->fetch();
-    		
-    		/*Récupérer l'ID de l'agent connecté*/
-    		$agent_id=$reponse['id'];
-    		
-    		$req_rapp=$bdd->prepare('SELECT * FROM clients inner join agents on clients.agent_id=:agentId ');
-    		
-    		$req_rapp->execute(array('agentId'=>$agent_id));
-    		
-    		$allRapport=$req_rapp->fetch();
-    		
-    		var_dump($allRapport);
-    
-    $json = json_decode(file_get_contents('php://input'), true);
-    
-    $phone = $json['phone'];
-    $response_array = array();
+    /*******************************************************************************************************************************************/
+    /*
+       1.SELECT FROM DATABASE.
+    */
+    public function search($query)
+    {
+        
+        $con=$this->connect();
+            
+            $requete=$con->prepare('SELECT * FROM agents WHERE phone=:phone');
+            
+            # Execution de la fonction
+            $requete->execute(array('phone'=>"5756351"));
+            $reponse=$requete->fetch();
+            
+            /*Récupérer l'ID de l'agent connecté*/
+            $agent_id=$reponse['id'];
 
-    try {
-
-    
-                            
-            $req_id=$bdd->query('SELECT id FROM clients ORDER BY id DESC LIMIT 1');
-            $id=$req_id->fetch();
-            $nocompte=$id['id'] + 1;
+        // $sql="SELECT * FROM clients WHERE agent_id=$agent_id ";
+        
+        
+            // $result=$con->query("SELECT * FROM clients WHERE agent_id=$agent_id");
             
-            $nocompte = sprintf("%06d", $nocompte);
+            $result=$con->prepare("SELECT * FROM clients WHERE agent_id=:ag and memdate=:date");
+            
+            // $result->execute(array('ag'=>1));
             
             
-            $response_array["status"] = "SUCCESS";
-            $response_array["noCompte"]=$nocompte;
+            if($result->execute(array('ag'=>$agent_id)))
+            {
+                $spacecrafts=array();
+                
+                while($row=$result->fetch())
+                {
+                    array_push($spacecrafts, array(
+                        "id"=>$row['id'],
+                        "clcode"=>$row['clcode'],
+                        "name"=>$row['name'],
+                        "surname"=>$row['surname'],
+                        "cnib"=>$row['cnib'],
+                        "photo"=>$row['photo']
+                        ));
+                }
+                print(json_encode(array_reverse($spacecrafts)));
+            }else
+            {
+                print(json_encode(array("Aucune réponse trouvée: ".$query)));
+            }
+            // $con->close();
 
         
-
-    } catch (Exception $e) {
-
-        $response_array["status"] = $e->getMessage();
+    }
+    public function handleRequest() {
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            
+            $query=$_POST['query'];
+            $this->search($query);    
+        } 
+        else{
+            $this->search("");
+        }
     
     }
-
-
-
-echo json_encode($response_array);
-
-exit();
-
-
-
-
+}
+$spacecrafts=new Spacecrafts();
+$spacecrafts->handleRequest();
+//end
